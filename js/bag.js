a9fa1af2db8ecb4b5adaf43b3c22a031e8346fad
Yizhou Wang <yizhouw@nvidia.com>
2022-06-20 16:36:29 -0700

    transmission

--- js/bag.js
+++ js/bag.js
@@ -1,28 +1,113 @@
 const { open } = require('rosbag');
+const sleep = require('util').promisify(setTimeout)
 
-async function logMessagesFromFooBar() {
-    // open a new bag at a given file location:
-    const bag = await open('../demo.bag');
+let startTime = { sec: 1490150297, nsec: 0 };  // 1490150297
+let lastTime = { sec: startTime.sec, nsec: startTime.nsec };
 
-    // read all messages from both the '/foo' and '/bar' topics:
-    await bag.readMessages({
-        topics: ['/radar/tracks'],
-        startTime: { sec: 1490150297, nsec: 0 }
-    }, (result) => {
-        // topic is the topic the data record was in
-        // in this case it will be either '/foo' or '/bar'
-        console.log(result.topic);
+class MessageStore {
+    constructor() {
+        this.store = new Array(0);
+        this.next = 0;
+    }
 
-        // message is the parsed payload
-        // this payload will likely differ based on the topic
-        console.log(result.message);
+    push(msg) {
+        this.store.push(msg);
+    }
 
-        // message's timestamp
-        console.log("message timestamp", result.timestamp)
-    });
+    length() {
+        return this.store.length;
+    }
 
-    console.log("bag end time", bag.endTime); // sec: 1490150297, nsec: 996942588
-    console.log("bag start time", bag.startTime); // sec: 1490150278, nsec: 1878520
+    consume() {
+        if (store.length != 0) {
+            return this.store.shift();
+        }
+    }
 }
 
-logMessagesFromFooBar();
+let store = new MessageStore();
+
+function delay(durationSec) {
+    return new Promise((resolve) => setTimeout(resolve, durationSec * 1000));
+}
+
+function timeDiff(timeA, timeB) {
+    return (timeB.sec - timeA.sec) * 1e9 + (timeB.nsec - timeA.nsec);
+}
+
+function getAllTopics({ connections }) {
+    let topics = new Array();
+
+    for (key in connections) {
+        topics[key] = connections[key].topic;
+    }
+
+    return topics;
+}
+
+function periodicPublish(store) {
+    void (async function (store) {
+
+        let lastTime = undefined;
+
+        while (true) {
+            const msg = store.consume();
+
+            if (lastTime && msg) {
+                const currentTime = msg.timestamp;
+                const delayTime = timeDiff(lastTime, currentTime) / 1e9;
+                console.log(delayTime);
+                await delay(delayTime);
+            }
+            else {
+                await delay(1);
+            }
+            if (msg != undefined) {
+                console.log(msg);
+                lastTime = Object.assign({}, msg.timestamp);
+            }
+            else {
+                console.log('no msg');
+            }
+        }
+    })(store);
+}
+
+async function logMessagesFromFooBar(store) {
+
+    // open a new bag at a given file location:
+    const bag = await open('../demo.bag');
+    startTime = bag.startTime;
+    topics = getAllTopics(bag);
+
+    await bag.readMessages({
+        topics: ["/image_raw"],
+        startTime: startTime
+    }, (result) => {
+
+        // don't print
+        if (false) {
+            // topic is the topic the data record was in
+            // in this case it will be either '/foo' or '/bar'
+            console.log(result.topic);
+
+            // message is the parsed payload
+            // this payload will likely differ based on the topic
+            // console.log(result.message);
+
+            // message's timestamp
+            console.log("message timestamp", result.timestamp)
+        }
+
+        store.push({
+            timestamp: result.timestamp,
+            message: result.message
+        });
+    });
+
+    // console.log("bag end time", bag.endTime); // sec: 1490150297, nsec: 996942588
+    // console.log("bag start time", bag.startTime); // sec: 1490150278, nsec: 1878520
+}
+
+const pro = logMessagesFromFooBar(store);
+periodicPublish(store)
